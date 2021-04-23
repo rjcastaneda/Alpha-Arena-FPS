f5e9743c9c288f19ed596adb16cc8f82d1cccfbc
Roswell James Castaneda <roswelljcastaneda@gmail.com>
2021-04-23 15:20:36 -0700

    Save: Added Health Bar, Script to Player HUD

---
+++ Assets/Scripts/Player/HUD/HealthBarHUD.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarHUD : MonoBehaviour
+{
+    //Health Bar Values
+    public float maxHealth;
+    public float currentHealth;
+
+    private Slider HealthSlider;
+
+    void Start()
+    {
+        HealthSlider = this.gameObject.GetComponent<Slider>();
+        //MaxHealth = PlayerData.maxHealth;
+        //CurrentHealth = PlayerData.currentHealth;
+    }
+
+    private void Update()
+    {
+        SetHealthBar();
+    }
+
+    public void SetHealthBar()
+    {
+        //currentHealth = PlayerData.currentHealth;
+        //MaxHealth = PlayerData.maxHealth;
+        HealthSlider.maxValue = maxHealth;
+        HealthSlider.value = currentHealth;
+    }
+}

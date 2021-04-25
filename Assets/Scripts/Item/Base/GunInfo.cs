765e68f39f9807ba4b967d3609bdc94751e844fd
John <jdavey@sdsu.edu>
2021-04-24 20:19:04 -0700

    Basic skeleton for generic item/weapon classes

---
+++ Assets/Scripts/Item/Base/GunInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Team 8 FPS Project/Create New Gun")]
+public class GunInfo : ItemInfo
+{
+    
+}

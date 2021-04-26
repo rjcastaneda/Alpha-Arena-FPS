c2f87d86e3945689fc7ad31370d819371563433c
John <jdavey@sdsu.edu>
2021-04-26 11:55:59 -0700

    Expanded upon GunInfo with more weapon specific variables

--- Assets/Scripts/Item/Base/GunInfo.cs
+++ Assets/Scripts/Item/Base/GunInfo.cs
@@ -5,6 +5,27 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Team 8 FPS Project/Create New Gun")]
 public class GunInfo : ItemInfo
 {
-    public float damage;
-    public float spread;    //Weapon accuracy
+    [Tooltip("Weapon damage.")]
+    public int damage;
+
+    [Tooltip("Rate of fire for the gun's primary attack.")]
+    public float rateOfFire;
+
+    [Tooltip("Rate of fire for the gun's alternate attack, if applicable.")]
+    public float rateOfFireAlt;
+
+    [Tooltip("Amount of rounds that can be stored in the weapon's magazine or clip.")]
+    public int magSize;
+
+    [Tooltip("Amount of rounds currently in the weapon's magazine or clip. This should be the same as mag size when the weapon is spawned.")]
+    public int currentMagSize;
+
+    [Tooltip("Index of the ammo pool that the weapon draws from. To be determined.")]
+    public int ammoType;
+
+    [Tooltip("Time it takes to finish reloading.")]
+    public float reloadTime;
+
+    [Tooltip("Time it takes to finish reloading if the weapon is empty.")]
+    public float reloadTimeEmpty;
 }

375dd89d5c81616363c3f56aa69b9182445be9be
Roswell James Castaneda <roswelljcastaneda@gmail.com>
2021-04-27 16:35:16 -0700

    Save: Working on Photon Player and Player Manager Scripts, updated PlayerData

---
+++ Assets/Scripts/Player/PhotonPlayer/PlayerManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerManager : MonoBehaviourPunCallbacks
+{
+    PhotonView PV;
+
+    void Awake()
+    {
+        PV = GetComponent<PhotonView>();
+    }
+
+    void CreatePlayer()
+    {
+
+    }
+
+    public void PlayerDeath()
+    {
+
+    }
+}
--- Assets/Scripts/Player/PlayerData/PlayerData.cs
+++ Assets/Scripts/Player/PlayerData/PlayerData.cs
@@ -1,51 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 //Script placed on player prefab
 //Holds values of the player, and performs functiosn related to those values
-public class PlayerData : MonoBehaviour
+public class PlayerData : MonoBehaviourPunCallbacks
 {
     public float maxHealth;
     public float health;
     public List<string> buffs;
     public bool isDead;
-
     private Vector3 playerPos;
 
     private void Awake()
     {
         playerPos = this.transform.position;
         isDead = false;
-    }
-
-    private void Update()
-    {
-        checkPlayerDeath();
-    }
-
-    void checkPlayerDeath()
-    {
-        if(isDead)
-        {
-            //Make player see death hud.
-            //Make sure player object is not destroyed
-            //Restore default values and respawn player.
-
-        }
-    }
-
-    public void resetPlayerHP() {
         health = maxHealth;
-        buffs.Clear();
     }
 
-    public void takeDamage(int damage)
+    //Keeps syncing data to players
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
-        health -= damage;
-        if(health < 0) {
-            isDead = true;
+        if (stream.IsWriting)
+        {
+            stream.SendNext(health);
+        }
+        else if (stream.IsReading)
+        {
+            health = (float)stream.ReceiveNext();
         }
     }
-
 }

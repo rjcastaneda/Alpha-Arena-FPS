f1ebe1227999ebc0440ef105422784284922b952
John <jdavey@sdsu.edu>
2021-04-27 15:45:48 -0700

    More inventory progress. Added equip/pickup item functions

--- Assets/Scripts/Player/Inventory/Player_Inventory.cs
+++ Assets/Scripts/Player/Inventory/Player_Inventory.cs
@@ -4,10 +4,49 @@ using UnityEngine;
 
 public class Player_Inventory : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Tooltip("Automatically swap to a new weapon when picked up.")]
+    [SerializeField] private bool AutoSwapNewWeapon = false;
+
+    [Tooltip("Weapon that the player starts with.")]
+    [SerializeField] private GameObject StartingWeapon;
+
+    [SerializeField] private List<Item> inventory = new List<Item>();
+
+    private GameObject inventoryObject;
+
+    int itemIndex;
+    int previousItemIndex = -1;
+
     void Start()
     {
-        
+        inventoryObject = transform.Find("PlayerCamera").Find("Inventory").gameObject;
+        PickUpItem(Instantiate(StartingWeapon));
+        EquipItem(0);
+    }
+
+    void EquipItem(int index)
+    {
+        itemIndex = index;
+        inventory[itemIndex].itemGameObject.SetActive(true);
+
+        if (previousItemIndex != -1)
+        {
+            inventory[previousItemIndex].itemGameObject.SetActive(false);
+        }
+
+        previousItemIndex = itemIndex;
+    }
+
+    void PickUpItem(GameObject obj)
+    {
+        obj.transform.SetParent(inventoryObject.transform);
+        obj.transform.localPosition = Vector3.zero;
+
+        inventory.Add(obj.GetComponent<StandardGun>());
+        if (AutoSwapNewWeapon)
+        {
+            EquipItem(inventory.Count - 1);
+        }
     }
 
     // Update is called once per frame

fb1ce23a8f2fb0e9454f6365cf6b3b854069b2ad
John <jdavey@sdsu.edu>
2021-04-27 16:01:29 -0700

    Added rifle. Implemented primary and secondary starting weapons. Added weapon swapping.

--- Assets/Scripts/Player/Inventory/Player_Inventory.cs
+++ Assets/Scripts/Player/Inventory/Player_Inventory.cs
@@ -7,25 +7,45 @@ public class Player_Inventory : MonoBehaviour
     [Tooltip("Automatically swap to a new weapon when picked up.")]
     [SerializeField] private bool AutoSwapNewWeapon = false;
 
-    [Tooltip("Weapon that the player starts with.")]
-    [SerializeField] private GameObject StartingWeapon;
+    [Tooltip("Primary weapon that the player starts with.")]
+    [SerializeField] private GameObject StartingWeaponPrimary;
+
+    [Tooltip("Secondary weapon that the player starts with.")]
+    [SerializeField] private GameObject StartingWeaponSecondary;
+
+    [Tooltip("The game object/point that weapons attach to.")]
+    [SerializeField] private GameObject inventoryObject;    //TODO rename this
 
     [SerializeField] private List<Item> inventory = new List<Item>();
-
-    private GameObject inventoryObject;
 
     int itemIndex;
     int previousItemIndex = -1;
 
     void Start()
     {
-        inventoryObject = transform.Find("PlayerCamera").Find("Inventory").gameObject;
-        PickUpItem(Instantiate(StartingWeapon));
-        EquipItem(0);
+        if (StartingWeaponPrimary != null)
+        {
+            PickUpItem(Instantiate(StartingWeaponPrimary));
+        }
+        if (StartingWeaponSecondary != null)
+        {
+            PickUpItem(Instantiate(StartingWeaponSecondary));
+        }
+        if (inventory.Count > 0)
+        {
+            EquipItem(0);
+        }
+        else
+            Debug.LogError("No primary or secondary starting weapon specified! Fix this!");
     }
 
     void EquipItem(int index)
     {
+        if (index == previousItemIndex)
+        {
+            return;
+        }
+
         itemIndex = index;
         inventory[itemIndex].itemGameObject.SetActive(true);
 
@@ -49,9 +69,16 @@ public class Player_Inventory : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //Loop to check for weapon swapping
+        for (int i = 0; i < inventory.Count; i++)
+        {
+            if (Input.GetKeyDown((i + 1).ToString()))
+            {
+                EquipItem(i);
+                break;
+            }
+        }
     }
 }

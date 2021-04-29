9d13ec378fe7cd53f1a8d8c76ed15270763f4f8a
Roswell James Castaneda <roswelljcastaneda@gmail.com>
2021-04-29 12:52:19 -0700

    Made menu nicer, added logo, added how to play

--- Assets/Scripts/MainMenuUI/MainMenu.cs
+++ Assets/Scripts/MainMenuUI/MainMenu.cs
@@ -12,7 +12,6 @@ public class MainMenu : MonoBehaviour
     public Button HowToPlayButton;
     private GameObject MainMenuCanvas;
     private GameObject HowToPlayCanvas;
-    private GameObject StartGameCanvas;
 
 
     // Start is called before the first frame update
@@ -20,7 +19,6 @@ public class MainMenu : MonoBehaviour
     {
         MainMenuCanvas = GameObject.Find("MainMenu");
         HowToPlayCanvas = GameObject.Find("HowToPlayMenu");
-        StartGameCanvas = GameObject.Find("StartGameMenu");
         ExitButton = MainMenuCanvas.transform.Find("ExitGameButton").gameObject.GetComponent<Button>();
         StartGameButton = MainMenuCanvas.transform.Find("StartButton").gameObject.GetComponent<Button>();
         HowToPlayButton = MainMenuCanvas.transform.Find("HowToPlayButton").gameObject.GetComponent<Button>();
@@ -32,7 +30,6 @@ public class MainMenu : MonoBehaviour
 
         //Set Defaults
         HowToPlayCanvas.SetActive(false);
-        StartGameCanvas.SetActive(false);
     }
 
     //Exit button functions
@@ -48,8 +45,6 @@ public class MainMenu : MonoBehaviour
     void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        //MainMenuCanvas.SetActive(false);
-        //StartGameCanvas.SetActive(true);
     }
 
     //How To Play Button function

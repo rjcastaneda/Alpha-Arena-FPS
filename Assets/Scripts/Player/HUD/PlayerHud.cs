15ab5df959fe29e11baad963d71e3814fdcb97ac
Feng Lin <flin2217@sdsu.edu>
2021-05-03 21:22:23 -0700

    Add countdown timer

--- Assets/Scripts/Player/HUD/PlayerHud.cs
+++ Assets/Scripts/Player/HUD/PlayerHud.cs
@@ -5,15 +5,44 @@ using UnityEngine.UI;
 
 public class PlayerHud : MonoBehaviour
 {
+    public Text timeText;
+    public float timeRemaining = 90;
+    public bool timerIsRunning = false;
    
     void Start()
     {
+        timerIsRunning = true;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+            }
+            else
+            {
+                Debug.Log("Time has run out!");
+                timerIsRunning = false;
+                timeRemaining = 0;
+            }
+        }
     }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay += 1;
+        
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+    }
+
 }
